0d9aaeb0d81b087bed725a3a1a1279aee6f66c8e
MoYoussouf <mohammmedaminey@gmail.com>
2024-12-18 15:37:14 +0100

    Fix issues with Jest and node-fetch

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   collectCoverage: true, // Collect test coverage
   collectCoverageFrom: ['server.js'], // Ensure coverage collection from the server.js file
-  testRegex: ".*\\.test\\.js$",
   coverageDirectory: 'coverage', // Output directory for the coverage reports
   coverageReporters: ['text', 'lcov'], // Report in text and lcov formats
   testRegex: '.*\\.test\\.js$', // Match files with .test.js for testing
